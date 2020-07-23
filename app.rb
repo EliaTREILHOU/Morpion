047b6c25507bc4a9be62f5700b54eff40b09d33b
EliaTREILHOU <67633877+EliaTREILHOU@users.noreply.github.com>
2020-07-23 19:13:03 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,17 +1,25 @@
-require "bundler"
+require 'bundler'
 Bundler.require
-require_relative '/lib/app/broad'
-require_relative '/lib/app/braodcase'
-require_relative '/lib/app/game'
-require_relative '/lib/app/player'
 
-class morpion
-  def perform
-  player
-  boardcase
-  board
-  game
-end
-end
+require_relative 'lib/board.rb'
+require_relative 'lib/boardcase.rb'
+require_relative 'lib/player.rb'
+require_relative 'lib/game.rb'
 
-morpion.new.perform
+puts "Bienvenue dans mon jeu du Morpion"
+
+puts "Entrez un nom pour le joueur 1 : "
+print "> "
+name_player1 = gets.chomp
+puts "Entrez un nom pour le joueur 2 : "
+print "> "
+name_player2 = gets.chomp
+system "clear"
+
+my_game = Game.new(name_player1, name_player2)
+
+my_game.new_board_game
+my_game.move_player1
+my_game.move_player2
+my_game.execute_move_player1
+my_game.execute_move_player2
