287580283a2f121af95ad5d8fcb4e2ed9767564d
EliaTREILHOU <67633877+EliaTREILHOU@users.noreply.github.com>
2020-07-23 19:01:41 +0200

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,28 @@
+require_relative './boardcase.rb'
+
 class Board
     attr_accessor :row1, :row2, :row3, :col1, :col2, :col3, :diago1, :diago2 #lignes,colonnes, diagonales
 
-    def initialize
+    def initialize #mise en place du plateau
+        #ligne
         @row1 = [a1 = ' ', a2 = ' ', a3 = ' ']
         @row2 = [b1 = ' ', b2 = ' ', b3 = ' ']
         @row3 = [c1 = ' ', c2 = ' ', c3 = ' ']
+        #collone
         @col1 = [a1, b1, c1]
         @col2 = [a2, b2, c2]
         @col3 = [a3, b3, c3]
+        #diagonales
         @diago1 = [a1, b2, c3]
         @diago2 = [a3, b2, c1]
     end
 
-    def generate_board
+    def generate_board #interface ligne et colonne
         puts " "
-        puts  " #{a1 = ' '} | #{a2 = ' ' } | #{a3 = ' '} "
-        puts separator = "─"*11
-        puts " #{b1 = ' '} | #{b2 = ' '} | #{b3 = ' '} "
-        puts separator
-        puts " #{c1 = ' '} | #{c2 = ' '} | #{c3 = ' '} "
+        puts  " #{a1 = " " } | #{a2 = " " } | #{a3 = " "} "
+        puts separateur = "-----------"
+        puts " #{b1 = " "} | #{b2 = " "} | #{b3 = " "} "
+        puts separateur
+        puts " #{c1 = " "} | #{c2 = " "} | #{c3 = " "} "
     end
 end
