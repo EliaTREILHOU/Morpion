88bf0e315a31c5fbff241cd3163563ba5255a2fd
EliaTREILHOU <67633877+EliaTREILHOU@users.noreply.github.com>
2020-07-23 16:31:26 +0200

    Update boardcase.rb

--- lib/boardcase.rb
+++ lib/boardcase.rb
@@ -1,1 +1,17 @@
+class BoardCase
+  attr_reader :content
+  
+  def initialize(position)
+    # Ici on initialise une cellule du jeu
+    @position = position
+    @content = " "
+  end
 
+  def content_change(value)
+    @content = value
+  end
+
+  def color_change
+    @content = @content.colorize(:green)
+  end
+end
