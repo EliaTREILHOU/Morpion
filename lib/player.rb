55bcf2c6fbf31dc8fe40892d75f5e5d3af19c336
EliaTREILHOU <67633877+EliaTREILHOU@users.noreply.github.com>
2020-07-23 19:05:06 +0200

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
-
   attr_reader :name, :symbol
+  
   def initialize(name, symbol,nb_players = 0, nb_win = 0, nb_lose = 0)
     @name = name #nom
     @symbol = symbol #Symbole X ou O
@@ -30,6 +30,6 @@ class Player
     puts"First name" => @name
     puts "Number of game played" => @nb_players
     puts "Wins" => @nb_win
-    puts "Loss" => @nb_lose
+    puts "Lose" => @nb_lose
   end
 end
